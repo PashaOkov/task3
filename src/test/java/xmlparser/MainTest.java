1f2c67d76d126c0e1a658165b3feb57385c34f57
PashaOkov <pashaokov@yandex.ru>
2021-11-16 00:48:15 +0300

    v7

--- src/test/java/xmlparser/MainTest.java
+++ src/test/java/xmlparser/MainTest.java
@@ -35,4 +35,31 @@ class MainTest {
         Assertions.assertEquals(expected, outputStreamCaptor.toString());
     }
 
+    @Test
+    void ExactSearchInput() {
+        String[] args = {"-f", "test.xml", "-s", "file-1498940214.xhtml"};
+        Main.main(args);
+        String expected = "/dir-880176375/dir-2145307015/file-1498940214.xhtml\r\n";
+        Assertions.assertEquals(expected, outputStreamCaptor.toString());
+    }
+
+    @Test
+    void SimpleSearchInput() {
+        String[] args = {"-f", "test.xml", "-s", "‘*.java’"};
+        Main.main(args);
+        String expected = "/dir-880176375/file-1073842118.java\r\n" +
+                "/dir-880176375/file-123.java\r\n";
+        Assertions.assertEquals(expected, outputStreamCaptor.toString());
+    }
+
+    @Test
+    void ExtendedSearchInput() {
+        String[] args = {"-f", "test.xml", "-S", "‘.*?[a-z]{4}-\\d+.[a-z]+’"};
+        Main.main(args);
+        String expected = "/file-776194140.xml\r\n" +
+                "/dir-880176375/file-1073842118.java\r\n" +
+                "/dir-880176375/dir-2145307015/file-1498940214.xhtml\r\n" +
+                "/dir-880176375/file-123.java\r\n";
+        Assertions.assertEquals(expected, outputStreamCaptor.toString());
+    }
 }
